54fa99919ffb66cf55e4b9ca851a43caece79e10
AlexMarrer <alex.uscata80@gmail.com>
2025-01-28 20:12:44 +0100

    refactor: add uncommited code

--- Backend/RisingSigma/RisingSigma.API/Controllers/ExerciseController.cs
+++ Backend/RisingSigma/RisingSigma.API/Controllers/ExerciseController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using RisingSigma.Api.Logic;
 
 namespace RisingSigma.API.Controllers
 {
@@ -7,10 +8,12 @@ namespace RisingSigma.API.Controllers
     public class ExerciseController : ControllerBase
     {
         private readonly ILogger<ExerciseController> _logger;
+        private readonly IExerciseLogic _exerciseLogic;
 
-        public ExerciseController(ILogger<ExerciseController> logger)
+        public ExerciseController(ILogger<ExerciseController> logger, IExerciseLogic exerciseLogic)
         {
             _logger = logger;
+            _exerciseLogic = exerciseLogic;
         }
 
         [HttpGet]
