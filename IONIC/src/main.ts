f76e31ce7b9d4ded07f710df1397ed3b9989521d
FG1907 <frknguener@gmail.com>
2025-06-11 15:01:24 +0200

    refactor: tab

--- IONIC/src/main.ts
+++ IONIC/src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { DomSanitizer } from '@angular/platform-browser';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
